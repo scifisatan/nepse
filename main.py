14bd4c812aecb7b23210f8788613f122c18719f5
scifisatan <brawlershere@gmail.com>
2020-09-05 16:16:27 +0545

    .

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
+
 import requests
 from bs4 import BeautifulSoup
+
 headers = []
 url='https://merolagani.com/StockQuote.aspx'
-soup = BeautifulSoup(requests.get(url).text,"html.parser")
 response = dict()
-for i in soup.find_all("th"):
-    headers.append(i.text.strip())
-row_value = soup.find_all("tr")
-for i in row_value:
-    dict1 = dict()
-    try:
-        name = i.find("a",{"tabindex":"-1"}).get("title").strip()
-        dict1.update({'Name':name})
-    except:
-        pass
-    data = i.find_all("td")
-    for n,j in enumerate(data):
-        if headers[n].strip() != "#":
-            key = headers[n]
-            value = data[n].text.strip()
-            dict1.update({key:value})
-    try:
-        symbol = dict1.get('Symbol')
-        dict1.pop('Symbol')
-        response.update({symbol:dict1})
-    except:
-        pass
-print(response)
+
+def api(url):
+    soup = BeautifulSoup(requests.get(url).text,"html.parser")
+    for i in soup.find_all("th"):
+        headers.append(i.text.strip())
+    row_value = soup.find_all("tr")
+    for i in row_value:
+        dict1 = dict()
+        try:
+            name = i.find("a",{"tabindex":"-1"}).get("title").strip()
+            dict1.update({'Name':name})
+        except:
+            pass
+        data = i.find_all("td")
+        for n,j in enumerate(data):
+            if headers[n].strip() != "#":
+                key = headers[n]
+                value = data[n].text.strip()
+                dict1.update({key:value})
+        try:
+            symbol = dict1.get('Symbol')
+            dict1.pop('Symbol')
+            response.update({symbol:dict1})
+        except:
+            pass
+    return response
+
+
+print(api(url))

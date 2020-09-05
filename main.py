24a9bd9fb478b2cb76883c838d48009d89fd8ce1
scifisatan <brawlershere@gmail.com>
2020-09-05 13:27:18 +0545

    ..

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ import lxml.html as lh
 
 
 url='http://www.nepalstock.com/main/todays_price'
-#Create a handle, page, to handle the contents of the website
 page = requests.get(url)
-#Store the contents of the website under doc
 doc = lh.fromstring(page.content)
-#Parse data that are stored between <tr>..</tr> of HTML
 
-tr_elements = doc.xpath('//*[@id="home-contents"]/table')
-#Create empty list
+elements = doc.xpath('//*[@id="home-contents"]/table')
+
 data=""
 i=0
-#For each row, store each first element (header) and an empty list
-for t in tr_elements[0]:
+
+for t in elements[0]:
     i+=1
     name=t.text_content()
     data= data+name
